266f040a8bf2a13aef26fdcc1f3e13434c0f56d6
Piyush Shah <piyush.shah@espressif.com>
2020-09-06 18:20:39 +0530

    json_generator: Multiple changes and additions
    - All APIs prefix changed from json_ to json_gen_
    - Improvements in test code
    - LICENSE updated and also added to the source files
    - New APIs added to add a NULL element and also add long strings
    - Minor documentation fixes

--- json_generator.c
+++ json_generator.c
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2020 Piyush Shah <shahpiyushv@gmail.com>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -8,7 +24,7 @@
 #define MAX_INT_IN_STR  	24
 #define MAX_FLOAT_IN_STR 	30
 
-static inline int json_get_empty_len(json_str_t *jstr)
+static inline int json_gen_get_empty_len(json_gen_str_t *jstr)
 {
 	return (jstr->buf_size - (jstr->free_ptr - jstr->buf) - 1);
 }
@@ -16,14 +32,17 @@ static inline int json_get_empty_len(json_str_t *jstr)
 /* This will add the incoming string to the JSON string buffer
  * and flush it out if the buffer is full. Note that the data being
  * flushed out will always be equal to the size of the buffer unless
- * this is the last chunk being flushed out on json_end_str()
+ * this is the last chunk being flushed out on json_gen_end_str()
  */
-static int json_add_to_str(json_str_t *jstr, char *str)
+static int json_gen_add_to_str(json_gen_str_t *jstr, char *str)
 {
+    if (!str) {
+        return 0;
+    }
 	int len = strlen(str);
 	char *cur_ptr = str;
 	while (1) {
-		int len_remaining = json_get_empty_len(jstr);
+		int len_remaining = json_gen_get_empty_len(jstr);
 		int copy_len = len_remaining > len ? len : len_remaining;
 		memmove(jstr->free_ptr, cur_ptr, copy_len);
 		cur_ptr += copy_len;
@@ -35,7 +54,6 @@ static int json_add_to_str(json_str_t *jstr, char *str)
 			 * is registered
 			 */
 			if (!jstr->flush_cb) {
-				printf("Cannot flush. End of string");
 				return -1;
 			}
 			jstr->flush_cb(jstr->buf, jstr->priv);
@@ -47,10 +65,10 @@ static int json_add_to_str(json_str_t *jstr, char *str)
 }
 
 
-void json_str_start(json_str_t *jstr, char *buf, int buf_size,
-		json_flush_cb_t flush_cb, void *priv)
+void json_gen_str_start(json_gen_str_t *jstr, char *buf, int buf_size,
+		json_gen_flush_cb_t flush_cb, void *priv)
 {
-	memset(jstr, 0, sizeof(json_str_t));
+	memset(jstr, 0, sizeof(json_gen_str_t));
 	jstr->buf = buf;
 	jstr->buf_size = buf_size;
 	jstr->flush_cb = flush_cb;
@@ -58,179 +76,207 @@ void json_str_start(json_str_t *jstr, char *buf, int buf_size,
 	jstr->priv = priv;
 }
 
-void json_str_end(json_str_t *jstr)
+void json_gen_str_end(json_gen_str_t *jstr)
 {
 	*jstr->free_ptr = '\0';
 	if (jstr->flush_cb)
 		jstr->flush_cb(jstr->buf, jstr->priv);
-	memset(jstr, 0, sizeof(json_str_t));
+	memset(jstr, 0, sizeof(json_gen_str_t));
 }
 
-static inline void json_handle_comma(json_str_t *jstr)
+static inline void json_gen_handle_comma(json_gen_str_t *jstr)
 {
 	if (jstr->comma_req)
-		json_add_to_str(jstr, ",");
+		json_gen_add_to_str(jstr, ",");
 }
 
 
-static int json_handle_name(json_str_t *jstr, char *name)
+static int json_gen_handle_name(json_gen_str_t *jstr, char *name)
 {
-	json_add_to_str(jstr, "\"");
-	json_add_to_str(jstr, name);
-	return json_add_to_str(jstr, "\":");
+	json_gen_add_to_str(jstr, "\"");
+	json_gen_add_to_str(jstr, name);
+	return json_gen_add_to_str(jstr, "\":");
 }
 
 
-int json_start_object(json_str_t *jstr)
+int json_gen_start_object(json_gen_str_t *jstr)
 {
-	json_handle_comma(jstr);
+	json_gen_handle_comma(jstr);
 	jstr->comma_req = false;
-	return json_add_to_str(jstr, "{");
+	return json_gen_add_to_str(jstr, "{");
 }
 
-int json_end_object(json_str_t *jstr)
+int json_gen_end_object(json_gen_str_t *jstr)
 {
 	jstr->comma_req = true;
-	return json_add_to_str(jstr, "}");
+	return json_gen_add_to_str(jstr, "}");
 }
 
 
-int json_start_array(json_str_t *jstr)
+int json_gen_start_array(json_gen_str_t *jstr)
 {
-	json_handle_comma(jstr);
+	json_gen_handle_comma(jstr);
 	jstr->comma_req = false;
-	return json_add_to_str(jstr, "[");
+	return json_gen_add_to_str(jstr, "[");
 }
 
-int json_end_array(json_str_t *jstr)
+int json_gen_end_array(json_gen_str_t *jstr)
 {
 	jstr->comma_req = true;
-	return json_add_to_str(jstr, "]");
+	return json_gen_add_to_str(jstr, "]");
 }
 
-int json_push_object(json_str_t *jstr, char *name)
+int json_gen_push_object(json_gen_str_t *jstr, char *name)
 {
-	json_handle_comma(jstr);
-	json_handle_name(jstr, name);
+	json_gen_handle_comma(jstr);
+	json_gen_handle_name(jstr, name);
 	jstr->comma_req = false;
-	return json_add_to_str(jstr, "{");
+	return json_gen_add_to_str(jstr, "{");
 }
-int json_pop_object(json_str_t *jstr)
+int json_gen_pop_object(json_gen_str_t *jstr)
 {
 	jstr->comma_req = true;
-	return json_add_to_str(jstr, "}");
+	return json_gen_add_to_str(jstr, "}");
 }
-int json_push_array(json_str_t *jstr, char *name)
+int json_gen_push_array(json_gen_str_t *jstr, char *name)
 {
-	json_handle_comma(jstr);
-	json_handle_name(jstr, name);
+	json_gen_handle_comma(jstr);
+	json_gen_handle_name(jstr, name);
 	jstr->comma_req = false;
-	return json_add_to_str(jstr, "[");
+	return json_gen_add_to_str(jstr, "[");
 }
-int json_pop_array(json_str_t *jstr)
+int json_gen_pop_array(json_gen_str_t *jstr)
 {
 	jstr->comma_req = true;
-	return json_add_to_str(jstr, "]");
+	return json_gen_add_to_str(jstr, "]");
 }
 
-static int json_set_bool(json_str_t *jstr, bool val)
+static int json_gen_set_bool(json_gen_str_t *jstr, bool val)
 {
 	jstr->comma_req = true;
 	if (val)
-		return json_add_to_str(jstr, "true");
+		return json_gen_add_to_str(jstr, "true");
 	else
-		return json_add_to_str(jstr, "false");
+		return json_gen_add_to_str(jstr, "false");
 }
-int json_obj_set_bool(json_str_t *jstr, char *name, bool val)
+int json_gen_obj_set_bool(json_gen_str_t *jstr, char *name, bool val)
 {
-	json_handle_comma(jstr);
-	json_handle_name(jstr, name);
-	return json_set_bool(jstr, val);
+	json_gen_handle_comma(jstr);
+	json_gen_handle_name(jstr, name);
+	return json_gen_set_bool(jstr, val);
 }
 
-int json_arr_set_bool(json_str_t *jstr, bool val)
+int json_gen_arr_set_bool(json_gen_str_t *jstr, bool val)
 {
-	json_handle_comma(jstr);
-	return json_set_bool(jstr, val);
+	json_gen_handle_comma(jstr);
+	return json_gen_set_bool(jstr, val);
 }
 
-static int json_set_int(json_str_t *jstr, int val)
+static int json_gen_set_int(json_gen_str_t *jstr, int val)
 {
 	jstr->comma_req = true;
 	char str[MAX_INT_IN_STR];
 	snprintf(str, MAX_INT_IN_STR, "%d", val);
-	return json_add_to_str(jstr, str);
+	return json_gen_add_to_str(jstr, str);
 }
 
-int json_obj_set_int(json_str_t *jstr, char *name, int val)
+int json_gen_obj_set_int(json_gen_str_t *jstr, char *name, int val)
 {
-	json_handle_comma(jstr);
-	json_handle_name(jstr, name);
-	return json_set_int(jstr, val);
+	json_gen_handle_comma(jstr);
+	json_gen_handle_name(jstr, name);
+	return json_gen_set_int(jstr, val);
 }
 
-int json_arr_set_int(json_str_t *jstr, int val)
+int json_gen_arr_set_int(json_gen_str_t *jstr, int val)
 {
-	json_handle_comma(jstr);
-	return json_set_int(jstr, val);
+	json_gen_handle_comma(jstr);
+	return json_gen_set_int(jstr, val);
 }
 
 
-static int json_set_float(json_str_t *jstr, float val)
+static int json_gen_set_float(json_gen_str_t *jstr, float val)
 {
 	jstr->comma_req = true;
 	char str[MAX_FLOAT_IN_STR];
-	snprintf(str, MAX_FLOAT_IN_STR, "%f", val);
-	return json_add_to_str(jstr, str);
+	snprintf(str, MAX_FLOAT_IN_STR, "%.*f", JSON_FLOAT_PRECISION, val);
+	return json_gen_add_to_str(jstr, str);
 }
-int json_obj_set_float(json_str_t *jstr, char *name, float val)
+int json_gen_obj_set_float(json_gen_str_t *jstr, char *name, float val)
 {
-	json_handle_comma(jstr);
-	json_handle_name(jstr, name);
-	return json_set_float(jstr, val);
+	json_gen_handle_comma(jstr);
+	json_gen_handle_name(jstr, name);
+	return json_gen_set_float(jstr, val);
 }
-int json_arr_set_float(json_str_t *jstr, float val)
+int json_gen_arr_set_float(json_gen_str_t *jstr, float val)
 {
-	json_handle_comma(jstr);
-	return json_set_float(jstr, val);
+	json_gen_handle_comma(jstr);
+	return json_gen_set_float(jstr, val);
 }
 
-static int json_set_string(json_str_t *jstr, char *val)
+static int json_gen_set_string(json_gen_str_t *jstr, char *val)
 {
 	jstr->comma_req = true;
-	json_add_to_str(jstr, "\"");
-	json_add_to_str(jstr, val);
-	return json_add_to_str(jstr, "\"");
+	json_gen_add_to_str(jstr, "\"");
+	json_gen_add_to_str(jstr, val);
+	return json_gen_add_to_str(jstr, "\"");
 }
 
-int json_obj_set_string(json_str_t *jstr, char *name, char *val)
+int json_gen_obj_set_string(json_gen_str_t *jstr, char *name, char *val)
 {
-	json_handle_comma(jstr);
-	json_handle_name(jstr, name);
-	return json_set_string(jstr, val);
+	json_gen_handle_comma(jstr);
+	json_gen_handle_name(jstr, name);
+	return json_gen_set_string(jstr, val);
 }
 
-int json_arr_set_string(json_str_t *jstr, char *val)
+int json_gen_arr_set_string(json_gen_str_t *jstr, char *val)
 {
-	json_handle_comma(jstr);
-	return json_set_string(jstr, val);
+	json_gen_handle_comma(jstr);
+	return json_gen_set_string(jstr, val);
 }
 
-
-static int json_set_null(json_str_t *jstr)
+static int json_gen_set_long_string(json_gen_str_t *jstr, char *val)
 {
 	jstr->comma_req = true;
-	return json_add_to_str(jstr, "null");
-}
-int json_obj_set_null(json_str_t *jstr, char *name)
-{
-	json_handle_comma(jstr);
-	json_handle_name(jstr, name);
-	return json_set_null(jstr);
+	json_gen_add_to_str(jstr, "\"");
+	return json_gen_add_to_str(jstr, val);
 }
 
-int json_arr_set_null(json_str_t *jstr)
+int json_gen_obj_start_long_string(json_gen_str_t *jstr, char *name, char *val)
 {
-	json_handle_comma(jstr);
-	return json_set_null(jstr);
+	json_gen_handle_comma(jstr);
+	json_gen_handle_name(jstr, name);
+    return json_gen_set_long_string(jstr, val);
+}
+
+int json_gen_arr_start_long_string(json_gen_str_t *jstr, char *val)
+{
+	json_gen_handle_comma(jstr);
+    return json_gen_set_long_string(jstr, val);
+}
+
+int json_gen_add_to_long_string(json_gen_str_t *jstr, char *val)
+{
+    return json_gen_add_to_str(jstr, val);
+}
+
+int json_gen_end_long_string(json_gen_str_t *jstr)
+{
+    return json_gen_add_to_str(jstr, "\"");
+}
+static int json_gen_set_null(json_gen_str_t *jstr)
+{
+	jstr->comma_req = true;
+	return json_gen_add_to_str(jstr, "null");
+}
+int json_gen_obj_set_null(json_gen_str_t *jstr, char *name)
+{
+	json_gen_handle_comma(jstr);
+	json_gen_handle_name(jstr, name);
+	return json_gen_set_null(jstr);
+}
+
+int json_gen_arr_set_null(json_gen_str_t *jstr)
+{
+	json_gen_handle_comma(jstr);
+	return json_gen_set_null(jstr);
 }
--- test.c
+++ test.c
@@ -1,24 +1,44 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Copyright 2020 Piyush Shah <shahpiyushv@gmail.com>
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 #include <stdio.h>
-
+#include <string.h>
 #include <json_generator.h>
 
-void print_buf(char *buf, void *priv)
+static const char expected_str[] = "{\"first_bool\":true,\"first_int\":30,"\
+        "\"float_val\":54.16430,\"my_str\":\"new_name\",\"null_obj\":null,"\
+        "\"arr\":[[\"arr_string\",false,45.12000,null,25,{\"arr_obj_str\":\"sample\"}]],"\
+        "\"my_obj\":{\"only_val\":5}}";
+
+typedef struct {
+    char buf[256];
+    size_t offset;
+} json_gen_test_result_t;
+
+static void flush_str(char *buf, void *priv)
 {
-	printf("%s", buf);
+    json_gen_test_result_t *result = (json_gen_test_result_t *)priv;
+    if (result) {
+        if (strlen(buf) > sizeof(result->buf) - result->offset) {
+            printf("Result Buffer too small\r\n");
+            return;
+        }
+        memcpy(result->buf + result->offset, buf, strlen(buf));
+        result->offset += strlen(buf);
+    }
 }
 /* Creating JSON
 {
@@ -37,36 +57,54 @@ void print_buf(char *buf, void *priv)
     }
 }
 */
-int main(int argc, char **argv)
+
+static int json_gen_perform_test(json_gen_test_result_t *result, const char *expected)
 {
 	char buf[20];
-	printf("Creating JSON string [may require Line wrap enabled on console]\r\n");
-	json_str_t jstr;
-	json_str_start(&jstr, buf, sizeof(buf), print_buf, NULL);
-	json_start_object(&jstr);
-	json_obj_set_bool(&jstr, "first_bool", true);
-	json_obj_set_int(&jstr, "first_int", 30);
-	json_obj_set_float(&jstr, "float_val", 54.1643);
-	json_obj_set_string(&jstr, "my_str", "new_name");
-	json_obj_set_null(&jstr, "null_obj");
-	json_push_array(&jstr, "arr");
-	json_start_array(&jstr);
-	json_arr_set_string(&jstr, "arr_string");
-	json_arr_set_bool(&jstr, false);
-	json_arr_set_float(&jstr, 45.12);
-	json_arr_set_null(&jstr);
-	json_arr_set_int(&jstr, 25);
-	json_start_object(&jstr);
-	json_obj_set_string(&jstr, "arr_obj_str", "sample");
-	json_end_object(&jstr);
-	json_end_array(&jstr);
-	json_pop_array(&jstr);
-	json_push_object(&jstr, "my_obj");
-	json_obj_set_int(&jstr, "only_val", 5);
-	json_pop_object(&jstr);
-	json_end_object(&jstr);
-	json_str_end(&jstr);
-	printf("\r\n");
-	return 0;
+    memset(result, 0, sizeof(json_gen_test_result_t));
+	json_gen_str_t jstr;
+	json_gen_str_start(&jstr, buf, sizeof(buf), flush_str, result);
+	json_gen_start_object(&jstr);
+	json_gen_obj_set_bool(&jstr, "first_bool", true);
+	json_gen_obj_set_int(&jstr, "first_int", 30);
+	json_gen_obj_set_float(&jstr, "float_val", 54.1643);
+	json_gen_obj_set_string(&jstr, "my_str", "new_name");
+	json_gen_obj_set_null(&jstr, "null_obj");
+	json_gen_push_array(&jstr, "arr");
+	json_gen_start_array(&jstr);
+	json_gen_arr_set_string(&jstr, "arr_string");
+	json_gen_arr_set_bool(&jstr, false);
+	json_gen_arr_set_float(&jstr, 45.12);
+	json_gen_arr_set_null(&jstr);
+	json_gen_arr_set_int(&jstr, 25);
+	json_gen_start_object(&jstr);
+	json_gen_obj_set_string(&jstr, "arr_obj_str", "sample");
+	json_gen_end_object(&jstr);
+	json_gen_end_array(&jstr);
+	json_gen_pop_array(&jstr);
+	json_gen_push_object(&jstr, "my_obj");
+	json_gen_obj_set_int(&jstr, "only_val", 5);
+	json_gen_pop_object(&jstr);
+	json_gen_end_object(&jstr);
+	json_gen_str_end(&jstr);
+    if (strcmp(expected, result->buf) == 0) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
 
+int main(int argc, char **argv)
+{
+    json_gen_test_result_t result;
+	printf("Creating JSON string [may require Line wrap enabled on console]\r\n");
+    int ret = json_gen_perform_test(&result, expected_str);
+    printf("Expected: %s\r\n", expected_str);
+	printf("Generated: %s\r\n", result.buf);
+    if (ret == 0) {
+        printf("Test Passed!\r\n");
+    } else {
+        printf("Test Failed!\r\n");
+    }
+	return ret;
 }
